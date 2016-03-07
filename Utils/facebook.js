1cf110b53b5ac4a0cb6b162b1ad5a4c545887043
lapostoj <jerome.lapostolet@ensimag.grenoble-inp.fr>
2016-03-07 19:06:59 +0800

    Init Utils extension with Facebook script feature.
    The Facebook script mentioned enable me to clear the archive tab
    in the Saved feature of Facebook.
    (By default when deleting a post, it is archived instead of deleted).

---
+++ Utils/facebook.js
@@ -0,0 +1,14 @@
+/*
+* TO USE ON THE 'Archive' TAB OF THE 'Saved Articles' PAGE OF FACEBOOK (https://www.facebook.com/saved/?cref=28&collection_token=...)
+*/
+/* Get all the "..." button that expand in ["Unarchive", "Delete"] menu. */
+var inputs = document.getElementsByClassName('_42ft _4jy0 _p _4jy3 _517h _51sy'); 
+/* Just to know how many articles were deleted. */
+alert(inputs.length + ' articles will be deleted from the archive.');
+/* Click on the button to make the menus appear in the DOM */
+for(var i=0; i<inputs.length;i++) { inputs[i].click(); } 
+/* Get all the "Delete" element one by menu. We also get the "Unarchive". */
+var temp = document.getElementsByClassName('_54nh');
+/* Clic on the "Delete" elements. Every other two elements to skip "Unarchive ones". */
+for(var i=1; i<temp.length;i=i+2) { temp[i].parentElement.parentElement.click(); }
+/* Tested on 483 elements. A little long but it worked! */
